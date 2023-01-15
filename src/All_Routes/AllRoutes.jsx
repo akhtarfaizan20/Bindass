1aa814077049c0ce223729130aac7df9e64ba776
akhtarfaizan20 <akhtarfaizan20@gmail.com>
2023-01-15 13:58:09 +0530

    basic template created

---
+++ src/All_Routes/AllRoutes.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Homepage from "./Homepage";
+import CartPage from "./CartPage";
+
+import Products from "./Products";
+import Login from "./Login";
+import Signup from "./Signup";
+
+const AllRoutes = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/products/:target" element={<Products />} />
+        <Route path="/cart" element={<CartPage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default AllRoutes;
