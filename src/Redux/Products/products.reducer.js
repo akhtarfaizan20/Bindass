8542b17858cf4c772ff3f0258abf04341d90c70b
akhtarfaizan20 <akhtarfaizan20@gmail.com>
2023-01-18 15:09:16 +0530

    redux template created

---
+++ src/Redux/Products/products.reducer.js
@@ -0,0 +1,37 @@
+import * as types from "./products.types";
+
+const initState = {
+  loading: false,
+  error: false,
+  products: [],
+};
+
+export const productReducer = (state = initState, { type, payload }) => {
+  switch (type) {
+    case types.GET_PRODUCT_LOADING: {
+      return {
+        ...state,
+        loading: true,
+        error: false,
+      };
+    }
+    case types.GET_PRODUCT_ERROR: {
+      return {
+        ...state,
+        loading: false,
+        error: true,
+      };
+    }
+    case types.GET_PRODUCT_SUCCESS: {
+      return {
+        ...state,
+        loading: false,
+        error: false,
+        products: payload,
+      };
+    }
+    default: {
+      return state;
+    }
+  }
+};
