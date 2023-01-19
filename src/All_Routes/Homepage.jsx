b117e0eb173b5485e9fca8f7c40ca9021e176a78
sushant shekhar <sushant.shekhar151997@gmail.com>
2023-01-19 23:35:21 +0530

    update

--- src/All_Routes/Homepage.jsx
+++ src/All_Routes/Homepage.jsx
@@ -15,8 +15,8 @@ const Homepage = () => {
     {url:"https://images.bewakoof.com/uploads/grid/app/category-box-Oversize-tshirt-1672915214.png"},
     {url:"https://images.bewakoof.com/uploads/grid/app/category-box-Sweatshirt---Hoodies-1672915219.png"},
     {url:"https://images.bewakoof.com/uploads/grid/app/category-box-Sweater-1673010262.png"},
-    {url:"https://images.bewakoof.com/uploads/grid/app/category-box-printed-tshirt-men-1672915214.png"},
     {url:"https://images.bewakoof.com/uploads/grid/app/category-box-Jackets-1672915210.png"},
+    {url:"https://images.bewakoof.com/uploads/grid/app/category-box-Sweatshirt---Hoodies-women-1673010262.png"},
     {url:"https://images.bewakoof.com/uploads/grid/app/category-box-Full-Sleeve-Tshirt-1673010261.png"},
     {url:"https://images.bewakoof.com/uploads/grid/app/category-box-new-printed-tshirts-1668773246.jpg"},
     {url:"https://images.bewakoof.com/uploads/grid/app/category-box-Oversize-tshirt-women-1672915214.png"},
@@ -26,8 +26,24 @@ const Homepage = () => {
   
   
   ]
-  const [data1,setData]=useState(data)
-  console.log(data1);
+  const data2=[{url:"https://images.bewakoof.com/uploads/grid/app/revamped-banner-MID-SIZE-B1G1-02-1673359273.jpg"},
+  {url:"https://images.bewakoof.com/uploads/grid/app/fullsleeves-tshirts-midsize-banner-1674050707.jpg"},
+  {url:"https://images.bewakoof.com/uploads/grid/app/midsize-banners-air-ovrszd-cargo-jogs-m-1674133076.jpg"},
+  {url:"https://images.bewakoof.com/uploads/grid/app/new-mid-banner-2023-printedsweatshoodsj-offer-1673631219.jpg"},    
+]
+const data4=[
+  {url:"https://images.bewakoof.com/uploads/grid/app/category-box-Jeans-men-1672915059.png"},
+  {url:"https://images.bewakoof.com/uploads/grid/app/category-box-new-joggers-1668772677.jpg"},
+  {url:"https://images.bewakoof.com/uploads/grid/app/category-box-new-pyjamas-1668772679.jpg"},
+  {url:"https://images.bewakoof.com/uploads/grid/app/category-box-new-2-1669360792.jpg"},
+   {url:"https://images.bewakoof.com/uploads/grid/app/category-box-new-joggers-1668773245.jpg"},
+   {url:"https://images.bewakoof.com/uploads/grid/app/category-box-Dress-1672907485.png"},
+
+]
+  const [data1,setData1]=useState(data)
+  const [data3,setData3]=useState(data2)
+  const [data5,setData4]=useState(data4)
+  console.log(data1[0].url);
   const breakpoints=[
     {
       width:1,itemsToShow:1
@@ -58,23 +74,23 @@ const breakpoints1=[
   return (
     <div>
       <div style={{marginTop:"15px"}}>
-    <Carousel breakPoints={breakpoints} enableAutoPlay autoPlaySpeed={2000} enableMouseSwipe  enableSwipe   showArrows={false}>
-      <Item><Img src="https://images.bewakoof.com/uploads/grid/app/mad-india-common--1673630619.jpg" alt="bewakoof" height={"450px"} /></Item>
-      <Item><Img src="https://images.bewakoof.com/uploads/grid/app/revamped-banner-1x1-B3A899-new1-1674029532.jpg" alt="bewakoof" height={"450px"}/></Item>
-      <Item><Img src="https://images.bewakoof.com/uploads/grid/app/b2g1-1x1-common-1674035537.jpg" alt="bewakoof" height={"450px"}/></Item>
-      <Item><Img src="https://images.bewakoof.com/uploads/grid/app/static-1x1-clearance-teesunder399-1674036686.jpg" height={"450px"} alt="bewakoof"/></Item>
-      <Item><Img src="https://images.bewakoof.com/uploads/grid/app/winterwear-1x1-common-02-1674035538.jpg" height={"450px"} alt="bewakoof"/></Item>
-      <Item><Img src="https://images.bewakoof.com/uploads/grid/app/mad-india-common--1673630619.jpg" height={"450px"} alt="bewakoof" /></Item>
-      <Item><Img src="https://images.bewakoof.com/uploads/grid/app/revamped-banner-1x1-B3A899-new1-1674029532.jpg" height={"450px"} alt="bewakoof"/></Item>
-      <Item><Img src="https://images.bewakoof.com/uploads/grid/app/b2g1-1x1-common-1674035537.jpg" height={"450px"} alt="bewakoof"/></Item>
-      <Item><Img src="https://images.bewakoof.com/uploads/grid/app/static-1x1-clearance-teesunder399-1674036686.jpg" height={"450px"} alt="bewakoof"/></Item>
-      <Item><Img src="https://images.bewakoof.com/uploads/grid/app/winterwear-1x1-common-02-1674035538.jpg" height={"450px"} alt="bewakoof"/></Item>
+    <Carousel breakPoints={breakpoints} enableAutoPlay={true} enableTilt={true}  autoPlaySpeed={2000} enableMouseSwipe  enableSwipe={true}   showArrows={false}>
+      <Item><Img className='carouselsm' src="https://images.bewakoof.com/uploads/grid/app/mad-india-common--1673630619.jpg" alt="bewakoof" height={"450px"} /></Item>
+      <Item><Img className='carouselsm' src="https://images.bewakoof.com/uploads/grid/app/revamped-banner-1x1-B3A899-new1-1674029532.jpg" alt="bewakoof" height={"450px"}/></Item>
+      <Item><Img className='carouselsm' src="https://images.bewakoof.com/uploads/grid/app/b2g1-1x1-common-1674035537.jpg" alt="bewakoof" height={"450px"}/></Item>
+      <Item><Img className='carouselsm' src="https://images.bewakoof.com/uploads/grid/app/static-1x1-clearance-teesunder399-1674036686.jpg" height={"450px"} alt="bewakoof"/></Item>
+      <Item><Img className='carouselsm' src="https://images.bewakoof.com/uploads/grid/app/winterwear-1x1-common-02-1674035538.jpg" height={"450px"} alt="bewakoof"/></Item>
+      <Item><Img className='carouselsm' src="https://images.bewakoof.com/uploads/grid/app/mad-india-common--1673630619.jpg" height={"450px"} alt="bewakoof" /></Item>
+      <Item><Img className='carouselsm' src="https://images.bewakoof.com/uploads/grid/app/revamped-banner-1x1-B3A899-new1-1674029532.jpg" height={"450px"} alt="bewakoof"/></Item>
+      <Item><Img className='carouselsm' src="https://images.bewakoof.com/uploads/grid/app/b2g1-1x1-common-1674035537.jpg" height={"450px"} alt="bewakoof"/></Item>
+      <Item><Img className='carouselsm' src="https://images.bewakoof.com/uploads/grid/app/static-1x1-clearance-teesunder399-1674036686.jpg" height={"450px"} alt="bewakoof"/></Item>
+      <Item><Img className='carouselsm' src="https://images.bewakoof.com/uploads/grid/app/winterwear-1x1-common-02-1674035538.jpg" height={"450px"} alt="bewakoof"/></Item>
 
     </Carousel>
        </div>
-       <Img src="https://images.bewakoof.com/uploads/grid/app/Desktop-Strip-6-1669115141.jpg" alt="banner" width={"100%"} height="auto" style={{marginTop:"5px"}} className='images'/>
+       <Img  src="https://images.bewakoof.com/uploads/grid/app/Desktop-Strip-6-1669115141.jpg" alt="banner" className='images'/>
        <div style={{marginTop:"0px"}}>
-    <Carousel breakPoints={breakpoints1} enableSwipe showArrows={false} pagination={false} >
+    <Carousel breakPoints={breakpoints1} enableSwipe={true} showArrows={false} pagination={false} >
        <Item1><Img src="https://images.bewakoof.com/uploads/grid/app/new-thumbnail-icon-2022-bestseller-1671624963.jpg" alt="bewakoof"   /></Item1>
        <Item1><Img src="https://images.bewakoof.com/uploads/grid/app/Thumbnail-New-Arrivals-Common-1668508339.jpg" alt="bewakoof"   /></Item1>
        <Item1><Img src="https://images.bewakoof.com/uploads/grid/app/hotdeals-2-1668491210.jpg" alt="bewakoof"   /></Item1>
@@ -93,13 +109,41 @@ const breakpoints1=[
         <td><Img src="https://images.bewakoof.com/uploads/grid/app/DOTW-Split-banner-Desktop-Women-1674030025.jpg" alt="bindass"/></td>
       </tr>
     </table>
+    <center><b>TRENDING CATEGORIES</b></center>
     <div className='items'>
-{
-  data1.map(({url})=>{
-   <div><Img src={url} alt="bewakoof"/></div> 
-  })
-}
+    {
+      data1?.map(({url},i)=>(
+       <div key={i}><Img src={url} alt="bindass"/></div> 
+      ))
+    }
     </div>
+    <div className='items2'>
+    {
+      data3?.map(({url},i)=>(
+       <div key={i}><Img src={url} alt="bindass"/></div> 
+      ))
+    }
+    </div>
+    <div className='items3'>
+    {
+      data5?.map(({url},i)=>(
+       <div key={i}><Img src={url} alt="bindass"/></div> 
+      ))
+    }
+    </div>
+    <div style={{marginTop:"10px"}}>
+    <Carousel breakPoints={breakpoints} enableAutoPlay={true} enableTilt={true}  autoPlaySpeed={2000} enableMouseSwipe  enableSwipe={true}   showArrows={false}>
+      <Item><Img className='carouselsm' src="https://images.bewakoof.com/uploads/grid/app/hot-new-evrythng-1x1-01-1672806265.gif" alt="bewakoof" height={"450px"} /></Item>
+      <Item><Img className='carouselsm' src="https://images.bewakoof.com/uploads/grid/app/static-1x1-indo-common-1673685812.jpg" alt="bewakoof" height={"450px"}/></Item>
+      <Item><Img className='carouselsm' src="https://images.bewakoof.com/uploads/grid/app/puffer-40-off-1673631220.jpg" alt="bewakoof" height={"450px"}/></Item>
+      <Item><Img className='carouselsm' src="https://images.bewakoof.com/uploads/grid/app/ovrszd-tees-1x1-common1-1674129220.jpg" height={"450px"} alt="bewakoof"/></Item>
+      <Item><Img className='carouselsm' src="https://images.bewakoof.com/uploads/grid/app/hot-new-evrythng-1x1-01-1672806265.gif" alt="bewakoof" height={"450px"} /></Item>
+      <Item><Img className='carouselsm' src="https://images.bewakoof.com/uploads/grid/app/static-1x1-indo-common-1673685812.jpg" alt="bewakoof" height={"450px"}/></Item>
+      <Item><Img className='carouselsm' src="https://images.bewakoof.com/uploads/grid/app/puffer-40-off-1673631220.jpg" alt="bewakoof" height={"450px"}/></Item>
+      <Item><Img className='carouselsm' src="https://images.bewakoof.com/uploads/grid/app/ovrszd-tees-1x1-common1-1674129220.jpg" height={"450px"} alt="bewakoof"/></Item>
+      
+    </Carousel>
+       </div>
        </div>
   )
 }
