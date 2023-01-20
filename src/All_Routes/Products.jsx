7d3bbdd5edae568f9f3c9cbce532fe499c77ee57
SORDE12 <sordeomkar2564@gmail.com>
2023-01-20 11:47:19 +0530

    added

--- src/All_Routes/Products.jsx
+++ src/All_Routes/Products.jsx
@@ -1,86 +1,103 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router";
-import { getProducts } from "../Redux/Products/products.actions";
+import { getProducts, getTotalProducts } from "../Redux/Products/products.actions";
 import styles from "./Products.module.css";
 import ProductDiv from "../Components/Products/ProductDiv";
-import { Button } from "@chakra-ui/react";
+import { Button , Select } from "@chakra-ui/react";
 import Filter from "../Components/Products/Filter";
-
+import Pagination from "../Components/Products/Pagination";
 
 const Products = () => {
-  const { loading, error, products } = useSelector(
+  const [sort , setSort]=useState("")
+  const [order,setOrder]=useState("")
+  const [selectSort,setSelectSort]=useState("-")
+
+  const { loading, error, products  } = useSelector(
     (store) => store.productManager
   );
+  function handleClick(val){
+
+    setPage(val)
+  
+  }
   const { target } = useParams();
   const [page, setPage] = useState(1);
   const dispatch = useDispatch();
-    const limit=12;
-  console.log(target)
+  const limit = 12;
+  let totalPages=9
+  //console.log(target);
 
   useEffect(() => {
-    dispatch(getProducts({target, page,limit}));
-  }, [page,target]);
+    dispatch(getProducts({ target, page, limit ,sort , order}));
+    
+  }, [page, target, sort, order]);
 
   let name;
+//console.log(totalPages)
+  if (target === "men" ? (name = "Men") : (name = "Women"))
+    // console.log(name)
+    //console.log(products)
 
-  if (target==="men" ? name="Men" : name="Women")
- // console.log(name)
-//console.log(products)
-  return (
-    <div className={styles.productSection}>
-      <div className={styles.proTop}>
-        <p> Home  /{"  "}</p>
-      
-        <p>{" "} {name} {" "} Clothing</p>
-      </div>
 
-      <div className={styles.proMid}>
-        <div className={styles.proHeading}>
-            <h1>{name}{"  "} Clothing  </h1>
+    return (
+      <div className={styles.productSection}>
+        <div className={styles.proTop}>
+          <p> Home /{"  "}</p>
+
+          <p> {name} Clothing</p>
         </div>
-        <div className={styles.proBody}>
+
+        <div className={styles.proMid}>
+          <div className={styles.proHeading}>
+            <h1>
+              {name}
+              {"  "} Clothing{" "}
+            </h1>
+          </div>
+          <div className={styles.proBody}>
             <div className={styles.proFilter}>
               <div className={styles.filterHead}>
                 <h1>FILTERS</h1>
-              </div >
-              <div className={styles.filterBody}>
-                   <Filter />
               </div>
-
+              <div className={styles.filterBody}>
+                <Filter />
+              </div>
             </div>
             <div className={styles.proMainSec}>
-
               <div className={styles.proSortSec}>
-                <div style={{display:"flex", flexDirection:"row-reverse" , gap:"2rem"}}>
-                <select style={{border:"1px outline gray", borderRadius:"5px"}}>
-                  <option></option>
-                  <option>Price : High To Low</option>
-                  <option>Price : Low to High</option>
-                  <option>Popular</option>
-                  <option>New</option>
-                </select>
-                <h1 style={{fontSize:'16px'}}>Sort By</h1>
+                <div style={{ display: "flex", flexDirection: "row-reverse", gap: "2rem",}}>
+  
+                  <Select  size="md" variant="flushed" w={"200px"} onChange={(e)=>{
+                    let arr=e.target.value.split("-")
+                    setSort(arr[0])
+                    setOrder(arr[1])
+                    setSelectSort(e.target.value)
+                    
+                  }}>
+                    <option value="-">Popular</option>
+                    <option value="price-asc">Price: Low to High</option>
+                    <option value="price-desc">Price: High to Low</option>
+                    <option value="ratings-asc">Rating: Low to High</option>
+                    <option value="ratings-desc">Rating: High to low</option>
+                  </Select>
+                  <h1 style={{ fontSize: "16px" , padding:".5rem" }}>Sort By</h1>
                 </div>
               </div>
-              
+
               <div className={styles.productMain}>
-                {
-                  products.map((e)=>(
-                    <ProductDiv data={e} />
-                  ))
-                }
+                {products.map((e) => (
+                  <ProductDiv data={e} />
+                ))}
               </div>
-              <div style={{margin:"auto" , gap:"2rem" , padding:"2rem"}} >
-                <Button  colorScheme='teal' variant='outline' size='md' >Pre</Button>
-                <Button  colorScheme='teal' variant='outline' disabled="true" size='md'>{page}</Button>
-                <Button  colorScheme='teal' variant='outline' size='md'>Next</Button>
+              <div style={{ margin: "auto", gap: "2rem",  padding: "2rem" }}>
+                <Pagination page={page} totalPages={totalPages} handleClick={handleClick}/>
               </div>
-            </div> 
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
 };
 
 export default Products;
