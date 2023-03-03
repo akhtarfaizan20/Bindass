003dc3f193c2d57d377dfade5a82159ca9d4f920
akhtarfaizan20 <akhtarfaizan20@gmail.com>
2023-03-03 16:11:01 +0530

    search functionilties

--- src/Components/Navbar/Footer.jsx
+++ src/Components/Navbar/Footer.jsx
@@ -24,6 +24,7 @@ export default function LargeWithLogoLeft() {
     <Box
       bg={useColorModeValue("gray.50", "gray.900")}
       color={useColorModeValue("gray.700", "gray.200")}
+      zIndex="999"
     >
       <Container as={Stack} maxW={"6xl"} py={10}>
         <SimpleGrid
